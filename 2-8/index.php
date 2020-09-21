8262d1d6a2577d21983356f4fe6a0dec617a72e7
muna296 <70792160+muna296@users.noreply.github.com>
2020-09-21 14:42:00 +0900

    課題提出
    2－8作成

---
+++ 2-8/index.php
@@ -0,0 +1,9 @@
+<?php
+  $fruits = ["apple" => "りんご","orange" => "みかん","peach" => "もも"];
+
+  foreach($fruits as $key => $value){
+      echo $key;
+      echo "といったら".$value;
+      echo "<br>";
+  }
+?>
