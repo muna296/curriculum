f8c154aed5536ab92f0d2a026ab3e124f4b67bac
muna296 <70792160+muna296@users.noreply.github.com>
2020-09-21 19:11:23 +0900

    提出課題
    FizzBuzz問題

---
+++ 2-9/index.php
@@ -0,0 +1,15 @@
+<?php
+   for($i = 1; $i <= 100; $i++){
+       if($i % 3 == 0){
+           echo 'Fizz';
+       }elseif($i % 5 == 0){
+           echo 'Buzz';
+       }elseif(!($i & 3 == 0)&&!($i % 5 == 0)){
+           echo $i;
+       }
+           echo "\n";
+           echo "<br>";
+       
+       
+    }
+?>
