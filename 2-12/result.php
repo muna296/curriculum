e3e46e9a7072343069d25f3c06d7bbceb47a9c4e
muna296 <70792160+muna296@users.noreply.github.com>
2020-09-27 16:13:28 +0900

    課題提出
    いろいろなフォームの入力欄

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$syouhin = $_POST['syouhin'];
+$number = $_POST['number'];
+?>
+<p>私の名前：<?php echo $my_name; ?></p>
+<p>ご希望商品：<?php echo $syouhin; ?></p>
+<p>個数：<?php echo $number; ?></p>
